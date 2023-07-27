b6f0d0e70c944c2911cd30f6426185a0f9c4d7bc
murkulL <olegov23445@gamil.com>
2023-07-27 07:06:19 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+/* eslint-disable array-callback-return */
 // eslint-disable-next-line no-unused-vars
 import Card from './components/Card';
 // eslint-disable-next-line no-unused-vars
 import Drawer from './components/Drawer';
 // eslint-disable-next-line no-unused-vars
 import Header from './components/Header';
+
+const arr = [
+  { name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 11000, images: '/images/img-1.jpg' },
+  { name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 15000, images: '/images/img-2.jpg' },
+  { name: 'Кроссовки Puma X Aka Boku Future Rider', price: 12222, images: '/images/img-3.jpg' },
+  { name: 'Мужские Кроссовки Jordan Air Jordan 11', price: 12322, images: '/images/img-4.jpg' }
+
+];
 
 function App () {
   return (
@@ -19,7 +28,13 @@ function App () {
           </div>
           </div>
           <div className="d-flex">
-            <Card />
+            {arr.map((obj) => (
+              <Card
+              title={obj.name}
+              price={obj.price}
+              imageUrl={obj.images}
+              />
+            ))}
           </div>
         </div>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,22 @@
-function Card () {
-  return (
-    <><div className="card">
+function Card (props) {
+  const onClickButton = () => {
+    console.log(props.price);
+  };
+
+  return (<>
+  <div className="card">
       <div className="favorite">
         <img src="/images/svg/heart-unliked.svg" alt="heartUnliked icon" />
       </div>
 
-      <img width={133} height={112} src="/images/img-2.jpg" alt="product img" />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-      <div className="d-flex justify-between align-center">
+      <img width={133} height={112} src={props.imageUrl} alt="product img" />
+      <h5>{props.title}</h5>
+      <div className="d-flex align-center justify-between">
         <div className="d-flex flex-column">
           <span>Price:</span>
-          <b>1 200 $</b>
+          <b>{props.price} $</b>
         </div>
-        <button className="button"><img width={11} height={11} src="/images/svg/plus.svg" alt="plus button" /></button>
+        <button className="button" onClick={onClickButton}><img width={30} height={30} src="/images/svg/plus.svg" alt="plus button" /></button>
       </div>
     </div></>
   );
